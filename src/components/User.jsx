8f20edef0ab012c4a1cf6d274612adbcc3638165
srijatiwari <54482756+srijatiwari@users.noreply.github.com>
2024-05-14 17:20:23 +0530

    Update User.jsx

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -10,7 +10,7 @@ const User = (props) => {
             </div>
             <div class="flex flex-col items-center pb-10">
                 <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src={picture?.medium} alt=" profile" />
-                <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{name?.title} {name?.first || "N/A"} {name?.last}</h5>
+                <h5 class="mb-1 text-xl font-medium text-white">{name?.title} {name?.first || "N/A"} {name?.last}</h5>
                 <span class="text-sm text-gray-500 dark:text-gray-400">Age: {dob?.age || "N/A"}</span>
                 <div class="flex mt-4 md:mt-6">
                     <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{gender || "N/A" }</a>
